17574efde6dca80482100c771d8e162263face8d
Christian Marfia <christianmarfia@hotmail.com>
2022-05-12 16:55:52 +0200

    start

---
+++ anagram-start.js
@@ -0,0 +1,7 @@
+/**
+	* Scrivere una funzione che controlli se due parole sono anagrammi e mostrare
+	* il risultato in console.
+	* Esempio
+	* isAnagram("cinema", "iceman") => true
+	* isAnagram("manuel", "giraffa") => false
+	*/
